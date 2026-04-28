836fd87a6be8ba4a80e39e6ec41730844b456244
somesh123211 <someshninawe61@gmail.com>
2026-04-28 23:49:59 +0530

    fixed os import for render

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from timetable_core import generate_timetable
+import os
 
 app = Flask(__name__)
 
@@ -186,7 +187,7 @@ def check_clash():
 
 
 if __name__ == "__main__":
-   import os
+   
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
