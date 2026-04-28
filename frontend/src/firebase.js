06295570fdcb07186bf0eb6ab7e5cf7a6a9ed995
somesh123211 <someshninawe61@gmail.com>
2026-04-28 23:18:25 +0530

    removed firebase key from code

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCiCpjhFZhSfiLxO8ph4dv6ZHtSwW-IQc8",
-  authDomain: "timetable-system-ee069.firebaseapp.com",
-  projectId: "timetable-system-ee069",
-  storageBucket: "timetable-system-ee069.firebasestorage.app",
-  messagingSenderId: "101409529882",
-  appId: "1:101409529882:web:c4dba80f056f01595b31a3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
